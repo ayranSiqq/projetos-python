d413696d3d72b5ecc47bc85b3c50806576803df6
Ayran Siqueira <ayransiqq@gmail.com>
2026-04-25 22:26:36 -0300

    tirei adiçoes indesejadas
    git colocou as duas versoes aq, tirei a velha

--- calculadoras/calculadora otimizada22_04.py
+++ calculadoras/calculadora otimizada22_04.py
@@ -1,55 +1,4 @@
-<<<<<<< HEAD
-Novo = True
-while True:
-    if Novo:
-        print("potenciação: **, raiz quadrada: r")
-        try:
-            n1 = float(input("Digite o primeiro termo:"))
-        except ValueError:
-            print("Entrada inválida! Por favor, digite um número.")
-            continue
-    ope = input("Digite a operação desejada, ou 'não' para sair: ")
-    if ope == "não":
-        Novo = True
-        continue
-    if ope != "r":
-        try:
-            n2 = float(input("Digite o segundo termo:"))
-        except ValueError:
-            print("Entrada inválida! Por favor, digite um número.")
-            continue
 
-    if ope == "+":
-        R = n1 + n2
-        print("O resultado é:", R)
-    elif ope == "-":
-        R = n1 - n2
-        print("O resultado é:", R)
-    elif ope == "*":
-        R = n1 * n2
-        print("O resultado é:", R)
-    elif ope == "/":
-        if n2 == 0:
-            print("Erro: divisão por zero!")
-            continue
-        R = n1 / n2
-        print("O resultado é:", R)
-    elif ope == "**":
-        R = n1 ** n2
-        print("O resultado é:", R)
-    elif ope == "r":
-        R = n1 ** 0.5   
-        print("O resultado é:", R) 
-    else:
-        print("Operação inválida!")
-        continue
-    escolha = input("Deseja continuar? Digite 'sim' ou 'não': ")
-    if escolha == "sim":
-        n1 = R
-        Novo = False
-    elif escolha == "não":
-        break
-=======
 import math
 print("potenciação: **, raiz quadrada: r")
 Novo = True
@@ -106,5 +55,3 @@ while True:
         continue
     n1 = R
     Novo = False
-
->>>>>>> 281d870 (adicionei log, porcentagem e removi falhas)
