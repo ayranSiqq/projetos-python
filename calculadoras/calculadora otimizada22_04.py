77e3fc64cb5cf8c44a0435a1f6f83d0b7c3d49ae
Ayran <ayransiqq@gmail.com>
2026-04-25 22:05:16 -0300

    adicionei log, porcentagem e removi falhas

--- calculadoras/calculadora otimizada22_04.py
+++ calculadoras/calculadora otimizada22_04.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 Novo = True
 while True:
     if Novo:
@@ -48,3 +49,62 @@ while True:
         Novo = False
     elif escolha == "não":
         break
+=======
+import math
+print("potenciação: **, raiz quadrada: r")
+Novo = True
+while True:
+    if Novo:
+        try:
+            n1 = float(input("Digite o primeiro termo:"))
+        except ValueError:
+            print("Entrada inválida! Por favor, digite um número.")
+            continue
+    ope = input("Digite a operação desejada, ou 'não' para sair: ")
+    if ope == "não":
+        Novo = True
+        continue
+    if ope != "r":
+        try:
+            n2 = float(input("Digite o segundo termo:"))
+        except ValueError:
+            print("Entrada inválida! Por favor, digite um número.")
+            continue
+
+    if ope == "+":
+        R = n1 + n2
+        print("O resultado é:", R)
+    elif ope == "%":
+        R = n1 * (n2 / 100)
+        print("O resultado é:", R)
+    elif ope == "-":
+        R = n1 - n2
+        print("O resultado é:", R)
+    elif ope == "*":
+        R = n1 * n2
+        print("O resultado é:", R)
+    elif ope == "/":
+        if n2 == 0:
+            print("Erro: divisão por zero!")
+            continue
+        R = n1 / n2
+        print("O resultado é:", R)
+    elif ope == "**":
+        R = n1 ** n2
+        print("O resultado é:", R)
+    elif ope == "r":
+        R = n1 ** 0.5   
+        print("O resultado é:", R) 
+    elif ope == "log":
+        if n1 <= 0:
+            print("Erro: logaritmo inválido!")
+            continue
+        R = math.log10(n1)
+        print("O resultado é:", R)
+    else:
+        print("Operação inválida!")
+        continue
+    n1 = R
+    Novo = False
+
+>>>>>>> 281d870 (adicionei log, porcentagem e removi falhas)
